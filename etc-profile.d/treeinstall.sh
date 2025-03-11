3b161420f8199c5cc7d421787cda089bc5d40b5e
carlosvalin94 <155858277+carlosvalin94@users.noreply.github.com>
2025-03-12 00:52:59 +0100

    Update treeinstall.sh

--- etc-profile.d/treeinstall.sh
+++ etc-profile.d/treeinstall.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Si esta variable ya está definida, significa que el script ya corrió en esta sesión
+if [[ -n "$TREEOS_UPDATE_DONE" ]]; then
+  return 0
+fi
+
+# Definimos la variable para que no se ejecute más veces en esta sesión
+export TREEOS_UPDATE_DONE=1
+
+# -- AQUI VA TU LOGICA --
 # Verificar si el repositorio ya fue clonado
 if [ ! -d "$HOME/.local/share/applications/.git" ]; then
   # Clonar el repositorio
@@ -12,4 +21,5 @@ else
 fi
 
 # Actualizar la ruta del icono en el archivo .desktop
-sed -i "s|Icon=TEMPATH.*|Icon=$HOME/.local/share/applications/treeos-control/logo.gif|" "$HOME/.local/share/applications/treeos-control.desktop"
+sed -i "s|Icon=TEMPATH.*|Icon=$HOME/.local/share/applications/treeos-control/logo.gif|" \
+  "$HOME/.local/share/applications/treeos-control.desktop"
