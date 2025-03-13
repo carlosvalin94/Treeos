2662576a19081cc4d11941585466aea7d5ac48e5
carlosvalin94 <155858277+carlosvalin94@users.noreply.github.com>
2025-03-13 18:14:54 +0100

    Create install-anaconda.sh

---
+++ treeos-control/install-anaconda.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Instalar dependencias
+sudo dnf install -y conda
+
+conda create -n env1 
+
+sudo dnf install qt5-qtbase qt5-qtbase-gui xcb-util xcb-util-wm xcb-util-image xcb-util-keysyms xcb-util-renderutil
+
+# Crear el archivo de acceso directo para Anaconda (modo Toolbox)
+echo -e "[Desktop Entry]\nName=Anaconda (Toolbox)\nComment=IDE para desarrollo de c�digo\nExec=toolbox run --container treeossecure conda anaconda-navigator\nIcon=code\nTerminal=false\nType=Application\nCategories=Development;IDE;" | tee ~/.local/share/applications/anaconda-treeossecure.desktop
+
+# Agregar permisos de ejecuci\ufffdn
+chmod +x ~/.local/share/applications/anaconda-treeossecure.desktop
