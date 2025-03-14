33f2ec9f462e4f0ab5f829c6585b55bcdffcdc69
carlosvalin94 <155858277+carlosvalin94@users.noreply.github.com>
2025-03-14 17:44:48 +0100

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# Instala los paquetes necesarios
-rpm-ostree install steam-devices openrgb-udev-rules gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-panel gnome-shell-extension-dash-to-dock gnome-shell-extension-appindicator openbox -r
-
-# Nombre del usuario a crear
+# Nombre del usuario temporal a crear
 USERNAME="treeostempus3rx01_script"
 
 # Crear el usuario sin contraseña
@@ -12,3 +9,8 @@ passwd -d "$USERNAME"
 
 # Agregar el usuario al grupo de administradores (wheel)
 usermod -aG wheel "$USERNAME"
+
+# Instala los paquetes necesarios
+rpm-ostree install steam-devices openrgb-udev-rules gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-panel gnome-shell-extension-dash-to-dock gnome-shell-extension-appindicator openbox -r
+
+
